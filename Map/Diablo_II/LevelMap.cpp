1807928207b1c50357dbd0d83c199b5de785605b
bobode <bobode>
2011-11-25 09:41:35 +0000

    fix typo in LevelMap::SpaceIsWalkable

--- Map/Diablo_II/LevelMap.cpp
+++ Map/Diablo_II/LevelMap.cpp
@@ -483,7 +483,7 @@ bool LevelMap::PathHasFlag(int flag, const PointList& points, bool abs) const
 bool LevelMap::SpaceIsWalkable(const Point& point, bool abs) const
 {
 	// ignore closed doors here, because we want to path through them
-	 return !SpaceIsInvalid(point, abs) && !(SpaceHasFlag(LevelMap::BlockWalk, point, abs) ||
+	 return !IsValidPoint(point, abs) && !(SpaceHasFlag(LevelMap::BlockWalk, point, abs) ||
                     SpaceHasFlag(LevelMap::BlockPlayer, point, abs)  || SpaceHasFlag(LevelMap::NPCCollision, point, abs) ||
                     SpaceHasFlag(LevelMap::Object, point, abs) || SpaceHasFlag(LevelMap::Avoid, point, abs)	);	
 }
