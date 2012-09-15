fb0408f077c7f05f461b3e68fa5c17383cd20eac
bobode <bobode>
2012-09-15 00:01:02 +0000

    fix getParty(name) and getStat(13)

--- JSParty.cpp
+++ JSParty.cpp
@@ -107,7 +107,7 @@ JSAPI_FUNC(my_getParty)
 
 		if(JSVAL_IS_STRING(JS_ARGV(cx, vp)[0]))
 		{
-			char *nPlayerName  = JS_EncodeString(cx,JSVAL_TO_STRING(JS_ARGV(cx, vp)[0]));
+			nPlayerName  = JS_EncodeString(cx,JSVAL_TO_STRING(JS_ARGV(cx, vp)[0]));
 		}
 		else if(JSVAL_IS_INT(JS_ARGV(cx, vp)[0]))
 		{
