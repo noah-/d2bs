75208ce24377b1c957e5daa3ba327b480768e559
kolton <kolton>
2013-10-19 13:27:28 +0000

    script stop fix

--- D2BS.h
+++ D2BS.h
@@ -5,7 +5,7 @@
 
 #define XP_WIN
 
-#define D2BS_VERSION "1.5.1796" //uptodate with d branch 1765 ff 20b
+#define D2BS_VERSION "1.5.1797" //uptodate with d branch 1765 ff 20b
 
 #include <windows.h>
 #include <vector>
