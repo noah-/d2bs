c9cd99964e0d5720c5660ab6f02b02272c9ed028
kolton <kolton>
2014-05-10 09:29:55 +0000

    Added missing locks to DisposeScript and getScripts

--- D2BS.h
+++ D2BS.h
@@ -5,7 +5,7 @@
 
 #define XP_WIN
 
-#define D2BS_VERSION "1.5.1825" //uptodate with d branch 1765 ff 20b
+#define D2BS_VERSION "1.5.1832" //uptodate with d branch 1765 ff 20b
 
 #include <windows.h>
 #include <vector>
